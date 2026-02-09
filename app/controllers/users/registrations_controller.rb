caf1b3c0e8a7b995e7a17e5016feef50d32d0266
cheemx5395 <chinmay.mahajan@joshsoftware.com>
2026-02-09 10:54:58 +0530

    patch: signup controller updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,6 +21,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.require(:registration).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
